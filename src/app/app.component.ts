02aa76bdc6c0221de5b8fafdfe1fd36e1bee00f9
jopiedra <68862066+Jopiedra@users.noreply.github.com>
2023-03-20 15:10:15 -0600

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Settings } from 'src/assets/settings';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular_app_amplify';
+
+  variable1 = process.env['NG_TEST_01'];
+  variable2 = process.env['NG_TEST_02'];
+  variable3 = process.env['NG_TEST_03'];
+
 }
---
+++ src/assets/settings.ts
@@ -0,0 +1,9 @@
+export const Settings = {
+    ApplyNow_API_BaseURL: process.env['NG_TEST_01'],
+    ApplyNow_UserName: "applynowdevusr",
+    ApplyNow_Password: "@pplyN0wD3v74",
+    Hubspot_API_BaseURL: "https://api.hsforms.com/submissions/v3/integration",
+    Hubspot_API_PortalID: "21749741",
+    Hubspot_API_FormGUID: "a7c3b934-fbe7-412d-ad65-3f72e7123cfe",
+    Encryptkey: "ApplyNowEncrypt2023"
+}
